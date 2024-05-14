d0b3c9c17457550ae03b68f5c3dddb4824268314
Mark <mrkcarlos576@gmail.com>
2024-05-14 19:29:46 +0300

    assignment complete

--- src/components/Filter.js
+++ src/components/Filter.js
@@ -1,9 +1,33 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 
-function Filter({ onCategoryChange }) {
+function Filter({ search, onSearchChange, onCategoryChange }) {
+  const [searchText, setSearchText] = useState(search);
+
+  useEffect(() => {
+    setSearchText(search);
+  }, [search]);
+
+  const handleSearchChange = (e) => {
+    const { value } = e.target;
+    setSearchText(value);
+    onSearchChange(value);
+  };
+
+  const filterItems = (items) => {
+    return items.filter(item =>
+      item.name.toLowerCase().includes(searchText.toLowerCase())
+    );
+  };
+
   return (
     <div className="Filter">
-      <input type="text" name="search" placeholder="Search..." />
+      <input
+        type="text"
+        name="search"
+        value={searchText}
+        onChange={handleSearchChange}
+        placeholder="Search..."
+      />
       <select name="filter" onChange={onCategoryChange}>
         <option value="All">Filter by category</option>
         <option value="Produce">Produce</option>
